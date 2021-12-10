434a838e87ada68640ad17588045c33df86b3d83
sayan95 <sayanmallik1995@gmail.com>
2021-12-10 21:40:10 +0530

    configures api versioning for skinet api project

---
+++ src/Skinet.API/Controllers/v1/BaseControllerV1.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Skinet.API.Controllers.v1
+{
+    [Route("api/v{version:apiVersion}/[controller]")]
+    [ApiVersion("1")]
+    [ApiController]
+    public class BaseControllerv1 : ControllerBase
+    {
+    }
+}
---
+++ src/Skinet.API/Extensions/ApplicationServiceExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Skinet.API.Extensions
+{
+    public static class ApplicationServiceExtensions
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            services.AddApiVersioning(options =>
+            {
+                options.DefaultApiVersion = Microsoft.AspNetCore.Mvc.ApiVersion.Default;
+                options.AssumeDefaultVersionWhenUnspecified = true;
+                options.ReportApiVersions = true;
+            });
+            return services;
+        }
+    }
+}
