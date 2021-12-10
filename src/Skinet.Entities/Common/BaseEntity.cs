c86af495e6cad41b7cd1f6ea6c5ed1f5d6d13e70
sayan95 <sayanmallik1995@gmail.com>
2021-12-10 21:43:32 +0530

    adds product entity for the shopper

---
+++ src/Skinet.Entities/Common/BaseEntity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Skinet.Entities.Common
+{
+    public class BaseEntity
+    {
+        public Guid Id { get; set; }
+        public string CreatedBy { get; set; }
+        public string LastModifiedBy { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime LastModifiedAt { get; set; }
+    }
+}
---
+++ src/Skinet.Entities/Entities/Product.cs
@@ -0,0 +1,9 @@
+﻿using Skinet.Entities.Common;
+
+namespace Skinet.Entities.Entities
+{
+    public class Product : BaseEntity
+    {
+        public string Name { get; set; }
+    }
+}
